10738f01baa3a5bef5f67ee9c63e794bdf82f049
Kathryn Schoephoester <kmschoep@usgs.gov>
2018-07-09 14:57:48 -0500

    MLR-274: add error handling to launch-app.sh

--- launch-app.sh
+++ launch-app.sh
@@ -3,6 +3,9 @@ set -e
 
 if [ -n "${OAUTH_CLIENT_SECRET_PATH}" ] && [ -f "${OAUTH_CLIENT_SECRET_PATH}" ]; then
   oauthClientSecret=$(cat ${OAUTH_CLIENT_SECRET_PATH})
+  else
+  	echo "oauthClientSecret is not defined"
+  	exit 0
 fi
 
 java -Djava.security.egd=file:/dev/./urandom -DoauthClientSecret=$oauthClientSecret -DkeystorePassword=$keystorePassword -jar app.jar $@
